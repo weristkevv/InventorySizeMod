a0db0dc3edcf810d56ae0d95544aabe24e02a854
Hydroo ! <nivek2006@web.de>
2024-08-04 21:36:52 +0200

    feat(settings):✨ added

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+  {
+    type = "int-setting",
+    name = "inventory-size",
+    setting_type = "runtime-per-user",
+    default_value = 80,
+    minimum_value = 20,
+    maximum_value = 2000,
+    order = "a"
+  }
+})
